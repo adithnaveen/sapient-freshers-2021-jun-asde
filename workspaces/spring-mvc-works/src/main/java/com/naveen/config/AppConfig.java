ef842c4f5789d637fe7f9e08b7626ccc3103e258
Naveen <adith.naveen@gmail.com>
2021-06-29 09:57:26 +0530

    added pom.xml  for spring mvc

---
+++ workspaces/spring-mvc-works/src/main/java/com/naveen/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.naveen.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.naveen.controller", "com.naveen.dao"})
+public class AppConfig implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext context; 
+		context = new AnnotationConfigWebApplicationContext(); 
+		context.register(AppConfig.class); 
+		
+		Dynamic servlet = servletContext.addServlet("ds", new DispatcherServlet(context)); 
+		servlet.addMapping("/"); 
+		servlet.setLoadOnStartup(1);
+		
+	}
+	
+}
